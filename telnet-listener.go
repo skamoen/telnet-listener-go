50a5340006dbe4570e716c457d0799d34bc8c8bb
Sille Kamoen <sille.kamoen@gmail.com>
2017-08-21 14:47:49 +0200

    Add metrics struct for full session logging

--- telnet-listener.go
+++ telnet-listener.go
@@ -19,6 +19,7 @@ var (
 	numberOfPorts  int
 	portStart      int
 	timeoutSetting time.Duration
+	sessionLog     *log.Logger
 )
 
 func init() {
@@ -29,9 +30,25 @@ func init() {
 
 	flag.Parse()
 
+	sessionLog = &log.Logger{
+		Formatter: new(log.JSONFormatter),
+		Hooks:     make(log.LevelHooks),
+		Level:     log.DebugLevel,
+	}
+
 	if *devPtr {
 		log.SetOutput(os.Stdout)
+		sessionLog.Out = os.Stdout
 	} else {
+		sessionLog.Formatter = &log.JSONFormatter{
+			TimestampFormat: "2006-01-02T15:04:05.999Z07:00",
+			FieldMap: log.FieldMap{
+				log.FieldKeyTime: "@timestamp",
+				// 		 FieldKeyLevel: "@level",
+				// 		 FieldKeyMsg: "@message",
+			},
+		}
+
 		log.SetFormatter(&log.JSONFormatter{
 			TimestampFormat: "2006-01-02T15:04:05.999Z07:00",
 			FieldMap: log.FieldMap{
@@ -47,6 +64,14 @@ func init() {
 		} else {
 			log.Info("Failed to log to file, using default stderr")
 		}
+
+		sessionfile, err := os.OpenFile("telnet-session.log", os.O_CREATE|os.O_WRONLY, 0666)
+		if err == nil {
+			sessionLog.Out = sessionfile
+
+		} else {
+			log.Info("Failed to log to file, using default stderr")
+		}
 	}
 
 	log.SetLevel(log.DebugLevel)
@@ -58,7 +83,7 @@ func main() {
 	sCount := 1
 	cchan := make(chan net.Conn, 100)
 
-	// Open 500 sockets on ports 23000 to 23499
+	// Open the specified number of ports from
 	for i := portStart; i < portStart+numberOfPorts; i++ {
 		go func(cchan chan net.Conn, i int) {
 			ln, err := net.Listen("tcp", ":"+strconv.Itoa(i))
@@ -102,6 +127,15 @@ func handleConnection(conn net.Conn, banner []byte, timeout time.Duration, sCoun
 		"session":     sCount,
 	})
 
+	metrics := new(metrics)
+	metrics.sessionStart = time.Now()
+	defer metrics.log()
+
+	metrics.sessionID = sCount
+	metrics.remoteIP = conn.RemoteAddr().(*net.TCPAddr).IP
+	metrics.remotePort = conn.RemoteAddr().(*net.TCPAddr).Port
+	metrics.localPort = conn.LocalAddr().(*net.TCPAddr).Port
+
 	conlog.Info("Accepted connection")
 
 	t := time.Now()
@@ -133,12 +167,15 @@ func handleConnection(conn net.Conn, banner []byte, timeout time.Duration, sCoun
 			return
 		}
 
-		conlog.WithFields(log.Fields{
-			"type":       "input",
-			"input_byte": buf[0],
-			"input_char": string(buf[0]),
-			"last_input": time.Since(lastInput).Nanoseconds() / 1000000,
-		}).Info("Input received")
+		// conlog.WithFields(log.Fields{
+		// 	"type":       "input",
+		// 	"input_byte": buf[0],
+		// 	"input_char": string(buf[0]),
+		// 	"last_input": time.Since(lastInput).Nanoseconds() / 1000000,
+		// }).Info("Input received")
+
+		metrics.input = append(metrics.input, buf[0])
+		metrics.inputTimes = append(metrics.inputTimes, time.Since(lastInput).Nanoseconds()/1000000)
 
 		switch buf[0] {
 		case 127: // DEL
@@ -155,7 +192,7 @@ func handleConnection(conn net.Conn, banner []byte, timeout time.Duration, sCoun
 		case 0: // null
 			fallthrough
 		case 10: // New Line
-			state = handleNewline(conn, state, &input, conlog)
+			state = handleNewline(conn, state, &input, metrics)
 		case 13:
 		default:
 			if state[0] == "username" {
@@ -172,11 +209,12 @@ func handleConnection(conn net.Conn, banner []byte, timeout time.Duration, sCoun
 	}
 }
 
-func handleNewline(conn net.Conn, state [3]string, input *bytes.Buffer, conlog *log.Entry) [3]string {
+func handleNewline(conn net.Conn, state [3]string, input *bytes.Buffer, metrics *metrics) [3]string {
 	if state[0] == "username" {
 		// Read all characters in the buffer
 		state[1] = input.String()
-		conlog.WithField("username", state[1]).Info("Username entered")
+		metrics.usernames = append(metrics.usernames, state[1])
+		// conlog.WithField("username", state[1]).Info("Username entered")
 
 		// Clear the buffer
 		input.Reset()
@@ -188,10 +226,12 @@ func handleNewline(conn net.Conn, state [3]string, input *bytes.Buffer, conlog *
 		// Store all characters in the buffer
 		state[2] = input.String()
 
-		conlog.WithFields(log.Fields{
-			"password": state[2],
-			"entry":    strings.Join(state[1:], ":"),
-		}).Info("Password entered")
+		metrics.passwords = append(metrics.passwords, state[2])
+		metrics.entries = append(metrics.entries, strings.Join(state[1:], ":"))
+		// conlog.WithFields(log.Fields{
+		// 	"password": state[2],
+		// 	"entry":    strings.Join(state[1:], ":"),
+		// }).Info("Password entered")
 
 		// Reset the buffers and state
 		input.Reset()
@@ -243,6 +283,38 @@ func negotiateTelnet(conn net.Conn) (err error) {
 		}
 	}
 	return nil
+}
+
+func (m *metrics) log() {
+
+	sessionLog.WithFields(log.Fields{
+		"session_id":       m.sessionID,
+		"session_start":    m.sessionStart,
+		"session_duration": time.Since(m.sessionStart) / 1000000,
+		"remote_ip":        m.remoteIP,
+		"local_port":       m.localPort,
+		"remote_port":      m.remotePort,
+		"input_bytes":      m.input,
+		"input_times":      m.inputTimes,
+		"usernames":        m.usernames,
+		"passwords":        m.passwords,
+		"entries":          m.entries,
+	}).Info("Session ended")
+
+}
+
+type metrics struct {
+	sessionStart    time.Time
+	sessionID       int
+	sessionDuration int
+	remoteIP        net.IP
+	localPort       int
+	remotePort      int
+	input           []byte
+	inputTimes      []int64
+	usernames       []string
+	passwords       []string
+	entries         []string
 }
 
 func logSessionTime(start time.Time, log *log.Entry) {
